0b8e639aafaa9d983776698da08f9a000e6719e4
Mahefa <hei.mahefa.2@gmail.com>
2026-03-20 00:41:05 +0300

    first commit

---
+++ src/main/java/com/example/TD3_springIntroduction/Td3SpringIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.example.TD3_springIntroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td3SpringIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td3SpringIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TD3_springIntroduction/Td3SpringIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TD3_springIntroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td3SpringIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
