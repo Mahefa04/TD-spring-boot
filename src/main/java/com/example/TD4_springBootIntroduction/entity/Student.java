89e26708fa2a380159111831f2e4c21bc448c3d1
Mahefa <hei.mahefa.2@gmail.com>
2026-03-26 09:05:54 +0300

    ajout des conteu de la clase Student

--- src/main/java/com/example/TD4_springBootIntroduction/entity/Student.java
+++ src/main/java/com/example/TD4_springBootIntroduction/entity/Student.java
@@ -1,4 +1,72 @@
 package com.example.TD4_springBootIntroduction.entity;
 
+import java.util.Objects;
+
 public class Student {
+    private String reference;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String reference, String firstName, String lastName, int age) {
+        this.reference = reference;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(reference, student.reference) && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reference, firstName, lastName, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "reference='" + reference + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
+
