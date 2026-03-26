96dea792c7c77cbee61fd9b6ba92cf0da7a242a2
Mahefa <hei.mahefa.2@gmail.com>
2026-03-26 08:43:32 +0300

    ajout des class vides StudentController et Student

---
+++ src/main/java/com/example/TD4_springBootIntroduction/Td4SpringBootIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.example.TD4_springBootIntroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td4SpringBootIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td4SpringBootIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/TD4_springBootIntroduction/controller/StudentController.java
@@ -0,0 +1,4 @@
+package com.example.TD4_springBootIntroduction.controller;
+
+public class StudentController {
+}
